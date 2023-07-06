2a26fdc0a05bbd0d72d3b30d33eafd85f884143e
renanakashima <renanakashima@g.ecc.u-tokyo.ac.jp>
2023-07-06 20:49:12 +0900

    changed

--- src/script.js
+++ src/script.js
@@ -5,3 +5,21 @@ document.querySelector('#header').innerHTML = message
 
 // Log to console
 console.log(message)
+
+let fruits = ["apple", "mango", "banana"];
+console.log(fruits);
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+
+function sumNumbers() {
+    let sum = 0;
+    for (let i = 1; i <= 100; i++) {
+        sum += i;
+    }
+    return sum;
+}
+
+console.log(sumNumbers());
+
